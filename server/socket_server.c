aff2834afab6541d59305339deb79555a0a9c0bc
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2017-06-04 14:02:56 +0200

    Cleanup of copied code
    * Warning for incompatible build instead of error
    * Removed trailing spaces
    * Renamed sockets to more descriptive names
    * Changed to my standard of bracket placement

--- server/socket_server.c
+++ server/socket_server.c
@@ -4,9 +4,9 @@
 
 #if defined(WIN32) || defined(__WIN32)
   #include <winsock.h>
-  #pragma comment(lib,"ws2_32.lib") //Winsock Library
+  #pragma comment(lib, "ws2_32.lib") //Winsock Library
 #else
-  #error "This won't build on other than Windows"
+  #warning "This is currently only for Windows"
   #include <sys/socket.h>
   #include <sys/un.h>
 #endif
@@ -14,65 +14,62 @@
 #define PRINT(...) printf(__VA_ARGS__); fflush(stdout);
 
 int main(int argc, char *argv[]) {
-  WSADATA wsa;
-  SOCKET s, new_socket;
+  WSADATA wsaData;
+  SOCKET sockListen = INVALID_SOCKET, sockAccept = INVALID_SOCKET;
   struct sockaddr_in server, client;
   int c;
   char *message;
 
   PRINT("\nInitialising Winsock...");
-  if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
-  {
-    PRINT("Failed. Error Code : %d",WSAGetLastError());
+  if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+    PRINT("WSAStartup failed. Error Code : %d", WSAGetLastError());
     return 1;
   }
-    
+
   PRINT("Initialised.\n");
-    
+
   //Create a socket
-  if((s = socket(AF_INET, SOCK_STREAM, 0 )) == INVALID_SOCKET)
-  {
+  if((sockListen = socket(AF_INET, SOCK_STREAM, 0 )) == INVALID_SOCKET) {
     PRINT("Could not create socket : %d", WSAGetLastError());
   }
 
   PRINT("Socket created.\n");
-    
+
   //Prepare the sockaddr_in structure
   server.sin_family = AF_INET;
   server.sin_addr.s_addr = INADDR_ANY;
   server.sin_port = htons( 8888 );
-    
+
   //Bind
-  if( bind(s, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
-  {
+  if (bind(sockListen, (struct sockaddr*)&server, sizeof(server)) == SOCKET_ERROR) {
     PRINT("Bind failed with error code : %d", WSAGetLastError());
   }
-    
+
   PRINT("Bind done\n");
 
   //Listen to incoming connections
-  listen(s, 3);
-    
+  listen(sockListen, 3);
+
   //Accept and incoming connection
   PRINT("Waiting for incoming connections...\n");
-    
+
   c = sizeof(struct sockaddr_in);
-  new_socket = accept(s, (struct sockaddr *)&client, &c);
-  if (new_socket == INVALID_SOCKET)
-  {
+  sockAccept = accept(sockListen, (struct sockaddr*)&client, &c);
+  if (sockAccept == INVALID_SOCKET) {
     PRINT("accept failed with error code : %d", WSAGetLastError());
   }
-    
+
   PRINT("Connection accepted\n");
 
   //Reply to client
   message = "Hello Client, I have received your connection. But I have to go now, bye\n";
-  send(new_socket, message, strlen(message), 0);
-    
+  send(sockAccept, message, strlen(message), 0);
+
   getchar();
 
-  closesocket(s);
+  closesocket(sockAccept);
+  closesocket(sockListen);
   WSACleanup();
-    
+
   return 0;
 }
